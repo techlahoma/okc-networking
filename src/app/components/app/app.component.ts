7fb5fea9ebc997eeeb745a3461e1e20a0a0b76a4
Austin W Keeton <austin.w.keeton@gmail.com>
2019-01-20 16:56:21 -0600

    Tabs have icons.

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'home',
-      sanitizer.bypassSecurityTrustResourceUrl('../../assets/home.svg'));
-  }
-}
+export class AppComponent { }
