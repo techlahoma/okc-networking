e00d46b7aeffbcc937be9c1f3a0999d2011e81f2
Austin W Keeton <austin.w.keeton@gmail.com>
2019-02-03 16:07:12 -0600

    Add routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,50 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent, SignupComponent, ScheduleComponent, LocationComponent, CurriculumComponent, FoodComponent, QuestionsComponent, SponsorsComponent } from './components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignupComponent
+  },
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  },
+  {
+    path: 'location',
+    component: LocationComponent
+  },
+  {
+    path: 'curriculum',
+    component: CurriculumComponent
+  },
+  {
+    path: 'food',
+    component: FoodComponent
+  },
+  {
+    path: 'questions',
+    component: QuestionsComponent
+  },
+  {
+    path: 'sponsors',
+    component: SponsorsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
