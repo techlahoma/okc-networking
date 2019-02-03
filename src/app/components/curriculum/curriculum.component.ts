372b218ef6e3b9b54b866919507daeb8907fe234
Austin W Keeton <austin.w.keeton@gmail.com>
2019-02-03 17:45:04 -0600

    Removed unneeded component.

--- src/app/components/curriculum/curriculum.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-curriculum',
-  templateUrl: './curriculum.component.html',
-  styleUrls: ['./curriculum.component.scss']
-})
-export class CurriculumComponent { }
